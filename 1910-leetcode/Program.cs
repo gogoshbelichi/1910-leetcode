7c5a25708b3280e9c20eaa72c60197a00483532e
Ivan Nesterov <85916405+gogoshbelichi@users.noreply.github.com>
2025-02-11 11:18:27 +0300

    do it

--- 1910-leetcode/Program.cs
+++ 1910-leetcode/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1910. Remove All Occurrences of a Substring
+//linq 
+//no func cases
+string s = "mountains, mountains, mountains, mountains";
+Console.WriteLine(s);
+s = s.Replace("mountains", "peaks"); // this replace all by one step
+Console.WriteLine(s);                // but it's case-sensitive thing.
+s = s.Replace("mountains", "peaks");
+Console.WriteLine(s);
+//func cases lol
+string zxc = "zzxclordzxclordxc";
+Console.WriteLine($"Removed Occurences of: ''zxc'' in string ''zxzxcclordzxclordxc'' => {RemoveOccurrences(zxc, "zxc")}");
+//case sensitive piece of crap which passed only 78/80 cases
+string RemoveOccurrences(string s, string part) {
+    if (part.Length > s.Length) return s;
+    while (AreThereOccurences(s, part))
+    {
+        s = s.Remove(s.IndexOf(part), part.Length);
+        s = s.Replace(part, "");
+        AreThereOccurences(s, part);
+    }
+    return s;
+}
+
+bool AreThereOccurences(string s, string part) => s.Contains(part);
